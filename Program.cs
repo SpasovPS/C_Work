be24d28cd0a584ff95c9aa0a2e0e62a6422b56b7
PavelSpasov <spasovy@yandex.ru>
2022-08-23 21:29:54 +0300

    error correction

--- Program.cs
+++ Program.cs
@@ -5,3 +5,16 @@ string[] array2;
 Console.WriteLine("Enter string:");
 count = 0;
 array = new string[count];
+do 
+{str = Console.ReadLine();
+    if (str!="")
+    {
+        count++; 
+        array2 = new string[count];
+        for (int i = 0; i < array2.Length - 1; i++)
+        array2[i] = array[i];
+        array2[count - 1] = str;
+        array = array2;
+    }
+} 
+while (str !="");
